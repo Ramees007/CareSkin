ee51a40abec085d1c74be82aaf3f084f7de5150c
shabnam-ua <142487441+shabnam-ua@users.noreply.github.com>
2025-12-31 12:40:28 +0530

    ios detail page

--- iosApp/iosApp/DetailPage.swift
+++ iosApp/iosApp/DetailPage.swift
@@ -6,7 +6,25 @@ struct DetailPage: View {
     var response: String
     
     var body: some View {
-        Text(response)
+        
+        VStack(alignment: .leading, spacing: 4) {
+            ForEach(response.components(separatedBy: "\n"), id: \.self) { line in
+                let attributed = try? AttributedString(markdown: line)
+                Text(attributed ?? AttributedString(""))
+                    .font(.title3)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+        .padding()
+     
+        Spacer()
     }
-    
 }
+
+struct DetailPage_Previews: PreviewProvider {
+    static var previews: some View {
+        let response = "**Dry Skin:**  \n- *Cleanser:* Hydrating cream cleanser.  \n- *Moisturizer:* Rich cream with ceramides/hyaluronic acid."
+        DetailPage(response: response)
+    }
+}
+
